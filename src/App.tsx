de4ee8f5404aa0fe957565957fa8caf2279cbcca
revanthsai1997 <revanthsaivarma@gmail.com>
2025-04-05 16:11:17 -0500

    useState, use Ref

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const listOfItems = ["car", "bike", "cycle"];
+  const [products, setProducts] = useState([{id:1, name:'dell'}])
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  const add = ()=>{
+    const maxId = products.length>0 ? Math.max(...products.map(x=>x.id)):0;
+    const newId = maxId +1;
+
+    setProducts([...products, { id: newId, name: inputElement.current?.value || 'Unnamed Product' }])
+  }
 
   return (
     <div>
-      <ul>{listOfItems.map(x=><li key={x}>{x}</li>)}</ul>
+      <input type='text' ref={inputElement}/>
+      <button onClick={add}>Add</button>
+      <ul>
+        {products.map(x=><li key={x.id}>{x.name}</li>)}
+      </ul>
     </div>
   )
 }
